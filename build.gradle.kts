43ccb9ecb7e2adff8eb2d4f9ed2eae98ce05d20a
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2022-11-08 10:35:01 +0100

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `java-library`
+    kotlin("jvm") version "1.7.20"
+    id("io.papermc.paperweight.userdev") version "1.3.8"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "at.clanattack"
+version = "0.1"
+
+bukkit {
+    main = "<Path to main>"
+    name = "Clanattack-<Name>"
+    version = "0.1"
+    apiVersion = "1.19"
+    author = "CheeseTastisch"
+}
+
+allprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "java")
+    apply(plugin = "java-library")
+    apply(plugin = "io.papermc.paperweight.userdev")
+    apply(plugin = "com.github.johnrengelman.shadow")
+
+    repositories {
+        mavenCentral()
+        maven("https://maven.clan-attack.at/releases")
+    }
+
+    dependencies {
+        paperDevBundle("1.19.2-R0.1-SNAPSHOT")
+        compileOnly("at.clanattack:Core:0.1")
+    }
+
+    tasks {
+        assemble {
+            dependsOn(reobfJar)
+        }
+
+        compileJava {
+            options.encoding = Charsets.UTF_8.name()
+            options.release.set(17)
+        }
+
+        javadoc {
+            options.encoding = Charsets.UTF_8.name()
+        }
+
+        processResources {
+            filteringCharset = Charsets.UTF_8.name()
+        }
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
